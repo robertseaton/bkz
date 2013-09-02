1f979c08383e2b3c0dfa4899034edefddc2864a3
Austin Walters <lettergramm@gmail.com>
2013-09-01 21:19:38 -0500

    Updated amazon.rb for additional author option and book rank
    Book rank is currently semi-broken and reports an amazon rank of zero for numbers less than 1,000 and greater than 1 million. There are currently 42 million books on amazon so that needs to be fixed.
    
    The --author option seems to work well, currently no errors to report.

--- amazon.rb
+++ amazon.rb
@@ -2,8 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 require 'net/http'
 
-def geturl(title)
-  return "http://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Dstripbooks&field-keywords=#{URI.encode(title)}"
+def geturl(title, author)
+
+	mytitle = title.dup 
+
+	if author != nil		
+		mytitle = mytitle << "+by+" << author
+	end
+
+  return "http://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Dstripbooks&field-keywords=#{URI.encode(mytitle)}"
 end
 
 def getresult(html, n)
@@ -15,17 +22,32 @@ def getresult(html, n)
   :url => result_0.css(".productTitle").css("a")[0]["href"]
   }
   return result
-end
+end		
 
 def getratings(html)
-  return {
-    :avg_rating => html.text.match("[1-5][.][1-5] out of 5 stars")[0].to_f,
-    :ratings_count => html.text.match("[0-9]* customer review")[0].to_i
+
+	rank = html.text.match("[0-9]*[,][0-9]* in Books").to_s
+	# known issue for amazon book rank seems like a syntax error <1M owrks and >1 thousand
+	rank = rank.delete('in Books').to_s
+	rank = rank.delete(',').to_i
+
+  ratings = {
+    :avg_rating => html.text.match("[1-5][.][0-5] out of 5 stars")[0].to_f,
+    :ratings_count => html.text.match("[0-9]* customer review")[0].to_i,
+		:ranking => rank
   }
+  return ratings
+rescue # In case there are no Amazon ratings...
+  puts "Didn't find Amazon ratings."
+  ratings = {
+    :avg_rating => 0,
+    :ratings_count => 0
+  }
+  return ratings
 end
 
-def amazon_search(title)
-  url = geturl(title)
+def amazon_search(title, author)
+  url = geturl(title, author)
   html = Nokogiri::HTML(open(url))
 
   result = getresult(html, 0)
